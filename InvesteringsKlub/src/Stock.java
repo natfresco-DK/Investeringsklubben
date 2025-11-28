836546aec7b4bfdc518548c58f37078f2cce11b9
Simon Nat Rignel Fresco <93189144+Natfresco@users.noreply.github.com>
2025-11-28 15:26:48 +0100

    created Stock class

---
+++ InvesteringsKlub/src/Stock.java
@@ -0,0 +1,19 @@
+public class Stock extends TradableItem {
+    protected String name;
+    protected String sector;
+
+    public Stock (String ticker, double price, String currency, String name, String sector){
+        super(ticker,price,currency);
+        this.name = name;
+        this.sector = sector;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+}
