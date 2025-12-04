c1df6fb3cefcb0a97254bb09f5dd8f68e2287158
10OliverF <ofolkersen@gmail.com>
2025-12-04 21:15:54 +0100

    Tilføjet portefølje for en user

--- InvesteringsKlub/src/App/Main.java
+++ InvesteringsKlub/src/App/Main.java
@@ -1,13 +1,5 @@
 package App;
 
-import Builder.PortfolioBuilder;
-import CSVHandler.CSVStockRepository;
-import CSVHandler.CSVTransactionRepository;
-import Domain.Portfolio;
-import Domain.User;
-
-import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starter programmet...");
