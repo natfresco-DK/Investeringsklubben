b5d0c7dc1274f605e40e4ba97990785f20ac4854
Simon Nat Rignel Fresco <93189144+Natfresco@users.noreply.github.com>
2025-12-12 07:47:52 +0100

    Fjernede ikke brugte metoder og udkommenteret kode

--- InvesteringsKlub/src/Builder/PortfolioBuilder.java
+++ InvesteringsKlub/src/Builder/PortfolioBuilder.java
@@ -23,28 +23,6 @@ public class PortfolioBuilder {
         return portfolio;
     }
 
-    public static void printPortfolio(User user, StockRepository stockRepo) {
-        Portfolio portfolio = user.getPortfolio();
-
-        System.out.println("\n===== PORTFOLIO FOR " + user.getFullName() + " =====");
-
-        System.out.println("CashBalance: " + portfolio.getCashBalance() + " DKK");
-        System.out.println("\nStock holdings:");
-
-        for (Holding h : portfolio.getHoldings().values()) {
-            System.out.println(
-                    "- " + h.getTicker() +
-                            ": " + h.getQuantity() + " qty. | " +
-                            "Purchase price: " + h.getPurchasePriceDKK() + " DKK | " +
-                            "Current price: " + h.getCurrentPriceDKK() + " DKK"
-            );
-        }
-
-        System.out.println("\nTotal invested: " + portfolio.calculateTotalInvestedDKK());
-        System.out.println("Total present value: " + portfolio.calculateCurrentHoldingsValueDKK(stockRepo));
-        System.out.println("Real return: " + portfolio.calculateRealReturnDKK(stockRepo));
-        System.out.println("Return in percent: " + portfolio.calculateReturnPercentage(stockRepo) + "%");
-    }
 
 
 
